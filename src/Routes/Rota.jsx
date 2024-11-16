8fbe13cb202141e13ba1358da405051075585539
ali-ali18 <alisson.camargo024@gmail.com>
2024-11-16 12:00:37 -0300

    esqueci do fallback

--- src/Routes/Rota.jsx
+++ src/Routes/Rota.jsx
@@ -2,7 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from '../Layouts/Layout';
 import LayoutFuncionario from '../Layouts/LayoutFuncionario';
 import { useSessionCheck } from '../Hooks/useSessionCheck';
-import React, { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
+import { Spinner } from '@nextui-org/react';
 
 const Home = React.lazy(() => import('../Pages/Home'));
 const Error404 = React.lazy(() => import('../Components/Error404'));
@@ -21,101 +22,103 @@ const Rota = () => {
 	useSessionCheck();
 
 	return (
-		<Routes>
-			{/* Area do cliente */}
-			<Route
-				path='/'
-				element={
-					<Layout>
-						<Home />
-					</Layout>
-				}
-			/>
-			<Route
-				path='/nossa-historia'
-				element={
-					<Layout>
-						<HistoryCardTeam />
-					</Layout>
-				}
-			/>
-			<Route
-				path='/seja-nosso-cliente'
-				element={
-					<Layout>
-						<SejaCliente />
-					</Layout>
-				}
-			/>
-			<Route
-				path='/posts'
-				element={
-					<Layout>
-						<AllPosts />
-					</Layout>
-				}
-			/>
-			<Route
-				path='/post/:id'
-				element={
-					<Layout>
-						<ViewPost />
-					</Layout>
-				}
-			/>
-			{/* Pagina de ADM na empresa */}
-			<Route path='/login' element={<Login />} />
-			<Route
-				path='/contato'
-				element={
-					<LayoutFuncionario>
-						<Contatos />
-					</LayoutFuncionario>
-				}
-			/>
-			<Route
-				path='/funcionario'
-				element={
-					<LayoutFuncionario>
-						<Funcionario />
-					</LayoutFuncionario>
-				}
-			/>
-			<Route
-				path='/dashboard-blog'
-				element={
-					<LayoutFuncionario>
-						<DashBoard />
-					</LayoutFuncionario>
-				}
-			/>
-			<Route
-				path='/new-post'
-				element={
-					<LayoutFuncionario>
-						<NewPost />
-					</LayoutFuncionario>
-				}
-			/>
-			<Route
-				path='/post-edit/:id'
-				element={
-					<LayoutFuncionario>
-						<PostEdit />
-					</LayoutFuncionario>
-				}
-			/>
+		<Suspense fallback={<Spinner color='success' label='Carregando, aguarde...' labelColor='success'/>}>
+			<Routes>
+				{/* Area do cliente */}
+				<Route
+					path='/'
+					element={
+						<Layout>
+							<Home />
+						</Layout>
+					}
+				/>
+				<Route
+					path='/nossa-historia'
+					element={
+						<Layout>
+							<HistoryCardTeam />
+						</Layout>
+					}
+				/>
+				<Route
+					path='/seja-nosso-cliente'
+					element={
+						<Layout>
+							<SejaCliente />
+						</Layout>
+					}
+				/>
+				<Route
+					path='/posts'
+					element={
+						<Layout>
+							<AllPosts />
+						</Layout>
+					}
+				/>
+				<Route
+					path='/post/:id'
+					element={
+						<Layout>
+							<ViewPost />
+						</Layout>
+					}
+				/>
+				{/* Pagina de ADM na empresa */}
+				<Route path='/login' element={<Login />} />
+				<Route
+					path='/contato'
+					element={
+						<LayoutFuncionario>
+							<Contatos />
+						</LayoutFuncionario>
+					}
+				/>
+				<Route
+					path='/funcionario'
+					element={
+						<LayoutFuncionario>
+							<Funcionario />
+						</LayoutFuncionario>
+					}
+				/>
+				<Route
+					path='/dashboard-blog'
+					element={
+						<LayoutFuncionario>
+							<DashBoard />
+						</LayoutFuncionario>
+					}
+				/>
+				<Route
+					path='/new-post'
+					element={
+						<LayoutFuncionario>
+							<NewPost />
+						</LayoutFuncionario>
+					}
+				/>
+				<Route
+					path='/post-edit/:id'
+					element={
+						<LayoutFuncionario>
+							<PostEdit />
+						</LayoutFuncionario>
+					}
+				/>
 
-			{/* Error em pag desconhecida... */}
-			<Route
-				path='*'
-				element={
-					<Layout>
-						<Error404 />
-					</Layout>
-				}
-			/>
-		</Routes>
+				{/* Error em pag desconhecida... */}
+				<Route
+					path='*'
+					element={
+						<Layout>
+							<Error404 />
+						</Layout>
+					}
+				/>
+			</Routes>
+		</Suspense>
 	);
 };
 
