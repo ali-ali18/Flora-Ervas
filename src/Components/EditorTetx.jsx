4c69fa9904adf3a54aa41ab6435961309844370b
ali-ali18 <alisson.camargo024@gmail.com>
2024-11-20 00:04:38 -0300

    correção de bugs no editar post

--- src/Components/EditorTetx.jsx
+++ src/Components/EditorTetx.jsx
@@ -1,48 +1,55 @@
-import React, { useRef, useEffect } from 'react';
-import Quill from 'quill';
-import 'quill/dist/quill.snow.css';
-
-
 const EditorText = ({ defaultContent = '', onChange }) => {
-	const editorRef = useRef(null); // Referência para o elemento onde o editor será montado
-	const quillInstance = useRef(null); // Referência para a instância do Quill
+	const editorRef = useRef(null);
+	const quillInstance = useRef(null);
 
 	useEffect(() => {
-		// Inicializa o editor Quill apenas uma vez
 		if (!quillInstance.current) {
 			quillInstance.current = new Quill(editorRef.current, {
 				theme: 'snow',
 				modules: {
 					toolbar: [
-						[{ header: [1, 2, false] }],
+						[{ header: [1, 2,3, false] }],
 						['bold', 'italic', 'underline'],
 						[{ list: 'ordered' }, { list: 'bullet' }],
 						['link', 'image'],
-						[{ align: [] }], // Adiciona alinhamento
+						[{ align: [] }],
 					],
 				},
 			});
 
-			// Define o conteúdo inicial, se fornecido
 			quillInstance.current.root.innerHTML = defaultContent;
 
-			// Configura o evento para capturar mudanças no texto
 			quillInstance.current.on('text-change', () => {
-				const content = quillInstance.current.root.innerHTML; // Pega o HTML atual do editor
-				onChange(content); // Chama a função onChange passada como prop
+				const content = quillInstance.current.root.innerHTML;
+				onChange(content);
 			});
-		} else {
-			// Atualiza o conteúdo quando defaultContent muda
+		}
+	}, [onChange]);
+
+	useEffect(() => {
+		if (
+			quillInstance.current &&
+			quillInstance.current.root.innerHTML !== defaultContent
+		) {
 			quillInstance.current.root.innerHTML = defaultContent;
 		}
-	}, [defaultContent, onChange]);
+	}, [defaultContent]);
+
+	useEffect(() => {
+		if (quillInstance.current) {
+			quillInstance.current.clipboard.addMatcher(
+				Node.ELEMENT_NODE,
+				(node, delta) => {
+					return delta; // Impede sobrescrita ao colar
+				},
+			);
+		}
+	}, []);
 
 	return (
 		<div
-			ref={editorRef} // Define a referência para o elemento div onde o editor será montado
+			ref={editorRef}
 			style={{ height: '400px', backgroundColor: 'white' }}
 		/>
 	);
 };
-
-export default EditorText;
