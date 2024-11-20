a2ea9b63ba451232abda03b252cfabb25afc3489
ali-ali18 <alisson.camargo024@gmail.com>
2024-11-20 16:36:38 -0300

    otimização do cards categoria

--- src/Components/CardImg.jsx
+++ src/Components/CardImg.jsx
@@ -1,12 +1,12 @@
 import { Card, CardHeader } from '@nextui-org/card';
 import { Image } from '@nextui-org/react';
 import { motion, useAnimation } from 'framer-motion';
-import { useEffect } from 'react';
+import { memo, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
-const CardImg = ({ categoria, caminho, produto }) => {
+const CardImg = memo(({ categoria, caminho, produto }) => {
 	const controls = useAnimation();
-	const [ref, inView] = useInView({ triggerOnce: false, threshold: 0.2 });
+	const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.2 });
 
 	useEffect(() => {
 		if (inView) {
@@ -23,29 +23,32 @@ const CardImg = ({ categoria, caminho, produto }) => {
 			initial='hidden'
 			variants={{
 				visible: { opacity: 1, y: 0 },
-				hidden: { opacity: 0, y: 20 },
+				hidden: { opacity: 0, y: 10 },
 			}}
-			transition={{ duration: 0.5 }}
+			transition={{ duration: 0.3 }}
 			className='min-w-[300px] w-[300px] h-[400px] overflow-hidden'
 		>
-			<Card className='w-full h-full overflow-hidden'>
-				<CardHeader className='absolute z-10 top-1 flex-col !items-start'>
-					<p className='text-tiny text-white/60 uppercase font-bold'>
-						{produto ||'Categoria'}
-					</p>
-					<h4 className='text-white font-medium text-2xl'>
-						{categoria || 'Sem categoria'}
-					</h4>
-				</CardHeader>
-				<Image
-					removeWrapper
-					alt={`foto do produto da categoria ${categoria ? categoria : 'Não especificada'}`}
-					className='z-0 object-cover w-full h-full'
-					src={caminho || 'https://nextui.org/images/card-example-3.jpeg'}
-				/>
-			</Card>
+			{inView && (
+				<Card className='w-[300px] h-[400px] overflow-hidden'>
+					<CardHeader className='absolute z-10 top-1 flex-col !items-start'>
+						<p className='text-tiny text-white/60 uppercase font-bold'>
+							{produto || 'Categoria'}
+						</p>
+						<h4 className='text-white font-medium text-2xl'>
+							{categoria || 'Sem categoria'}
+						</h4>
+					</CardHeader>
+					<Image
+						removeWrapper
+						alt={`foto do produto da categoria ${categoria || 'Não especificada'}`}
+						className='z-0 object-cover w-full h-full'
+						src={caminho || 'https://nextui.org/images/card-example-3.jpeg'}
+						lazyLoad
+					/>
+				</Card>
+			)}
 		</motion.div>
 	);
-};
+})
 
 export default CardImg;
